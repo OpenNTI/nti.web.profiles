7708cae8f09dbe6dc2b5ca15a030b6a66eaf2516
Cory Jones <cory.jones@nextthought.com>
2018-05-31 08:52:51 -0500

    Add body to empty test

--- src/__test__/example.spec.js
+++ src/__test__/example.spec.js
@@ -1,6 +1,8 @@
 /* eslint-env jest */
 describe('Exmaple', () => {
-	test('Example Skiped');
+	test('Example Skiped', () => {
+		expect(true).toBeTruthy();
+	});
 	test('Example Test', async () => {
 		//await somePromise()
 		expect(true).toBe(true);
