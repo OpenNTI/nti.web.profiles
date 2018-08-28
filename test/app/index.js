6b0bb78bd202725e5c13b91733a70f354ae2d37a
Ray Hatfield <ray.hatfield@nextthought.com>
2018-08-28 16:51:30 -0500

    provide router context so we can fake links to detail views

--- test/app/index.js
+++ test/app/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import ReactDOM from 'react-dom';
 import {getAppUser} from '@nti/web-client';
+import {encodeForURI} from '@nti/lib-ntiids';
+import {Router, Route as R, getHistory, LinkTo} from '@nti/web-routing';
 
 import {User} from '../../src';
 
@@ -8,6 +11,31 @@ window.$AppConfig = window.$AppConfig || {server: '/dataserver2/'};
 
 class Test extends React.Component {
 	state = {entity: null}
+
+	static contextTypes = {
+		router: PropTypes.object
+	};
+
+	static childContextTypes = {
+		router: PropTypes.object
+	}
+
+	getChildContext () {
+		const {router: nav} = this.context;
+		const router = {
+			...(nav || {}),
+			getRouteFor: this.getRouteFor,
+		};
+
+		return {
+			router
+		};
+	}
+
+	getRouteFor = (obj = {}, context) => {
+		const {router: {route}} = this.context;
+		return `${route.location.pathname}/${encodeForURI(obj.NTIID)}/#detail-view`;
+	}
 
 	async componentDidMount () {
 		const entity = await getAppUser();
@@ -26,9 +54,13 @@ class Test extends React.Component {
 	}
 }
 
-
+// Using a router just to provide context/childContext to
+// the component so it can simulate links to detail views
+const Rtr = Router.for([
+	R({path: '/', component: Test, name: 'nti-web-profiles-test'}),
+]);
 
 ReactDOM.render(
-	<Test />,
+	<Rtr />,
 	document.getElementById('content')
 );
