2f57a43581f09ff31690519a441b4e9279f82184
Andrew Ligon <andrew.ligon@nextthought.com>
2018-10-04 16:50:02 -0500

    Export some of the individual components for the user

--- src/user/index.js
+++ src/user/index.js
@@ -1,4 +1,6 @@
 export View from './View';
+export Header from './header';
+export About from './about';
 export ProfileCertificates from './certificates';
 export Transcripts from './transcripts';
 export CommunityCard from './communities';
