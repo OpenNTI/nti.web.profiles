47fb82c490e285e66d0526a72e7b5b78e88b2c0e
Ray Hatfield <ray.hatfield@nextthought.com>
2018-10-08 17:30:18 -0500

    let messages component connect to the store. avoid unnecessary re-rendering of the frame.

--- src/user/edit/Frame.jsx
+++ src/user/edit/Frame.jsx
@@ -1,33 +1,16 @@
 import React from 'react';
-import PropTypes from 'prop-types';
-import {Connectors} from '@nti/lib-store';
 
-import {ensureArray as a} from '../../util';
 import {Frame as AboutFrame} from '../about';
 
 import Messages from './messages';
-import {ERROR, FIELD_ERRORS} from './Store';
 
-export default
-@Connectors.Any.connect({
-	[ERROR]: 'error',
-	[FIELD_ERRORS]: 'fieldErrors'
-})
-class Frame extends React.Component {
-
-	static propTypes = {
-		children: PropTypes.any,
-		storeError: PropTypes.object,
-		fieldErrors: PropTypes.array
-	}
-
+export default class Frame extends React.PureComponent {
 	render () {
-		const {children, error, fieldErrors, ...other} = this.props;
-		const errors = error ? [error, ...a(fieldErrors)] : fieldErrors;
+		const {children, ...other} = this.props;
 
 		return (
 			<div className="nti-profile-edit-frame">
-				<Messages errors={errors} />
+				<Messages />
 				<AboutFrame {...other}>
 					{children}
 				</AboutFrame>
--- src/user/edit/messages/Messages.jsx
+++ src/user/edit/messages/Messages.jsx
@@ -1,16 +1,28 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import {Connectors} from '@nti/lib-store';
+
+import {ensureArray as a} from '../../../util';
+import {ERROR, FIELD_ERRORS} from '../Store';
 
 import getMessages from './factories';
 
-export default class Messages extends React.Component {
+export default
+@Connectors.Any.connect({
+	[ERROR]: 'error',
+	[FIELD_ERRORS]: 'fieldErrors'
+})
+class Messages extends React.Component {
 
 	static propTypes = {
-		errors: PropTypes.array
+		error: PropTypes.object,
+		fieldErrors: PropTypes.array
 	}
 
 	render () {
-		const {errors} = this.props;
+		const {error, fieldErrors} = this.props;
+		const errors = error ? [error, ...a(fieldErrors)] : fieldErrors;
+
 		const messages = getMessages(errors);
 
 		return !messages || messages.length === 0
