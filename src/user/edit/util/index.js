7d89c2c5903fcb855962407d28ba95c8c0372fb1
Ray Hatfield <ray.hatfield@nextthought.com>
2018-10-10 15:22:41 -0500

    remove unused

--- src/user/edit/util/index.js
+++ src/user/edit/util/index.js
@@ -1,4 +1,3 @@
 export addGroupsToSchema from './add-groups-to-schema';
 export getGroupedSchemaFields from './get-grouped-schema-fields';
-export groupSchemaFields from './group-schema-fields';
 export * as FieldConfig from './field-config';
