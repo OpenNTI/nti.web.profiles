84e2ba3616793c2ab7e9fcf3ddf8c6d3d7c17bca
Andrew Ligon <andrew.ligon@nextthought.com>
2018-10-17 13:06:19 -0500

    NTI-6843 use our dialog confirmation before leaving a profile with edits

--- src/user/edit/ConfirmExit.jsx
+++ src/user/edit/ConfirmExit.jsx
@@ -1,8 +1,16 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import {Prompt} from '@nti/web-routing';
+import {Prompt as RoutePrompt} from '@nti/web-routing';
+import {Prompt} from '@nti/web-commons';
+import {scoped} from '@nti/lib-locale';
 
 import {Store, HAS_UNSAVED_CHANGES} from './Store';
+
+const t = scoped('nti-profiles.user.edit.confirm-exit', {
+	unsavedChanges: 'You are currently editing your profile. Would you like to leave without saving?',
+	leave: 'Leave',
+	stay: 'Stay'
+});
 
 export default
 @Store.connect({
@@ -14,11 +22,25 @@ class ConfirmExit extends React.PureComponent {
 		hasUnsavedChanges: PropTypes.bool
 	}
 
+
+	onRoute = async (cont, stop) => {
+		try {
+			await Prompt.areYouSure(t('unsavedChanges'), void 0, {
+				confirmButtonLabel: t('leave'),
+				cancelButtonLabel: t('stay')
+			});
+
+			cont();
+		} catch (e) {
+			stop();
+		}
+	}
+
 	render () {
 		const {hasUnsavedChanges = false} = this.props;
 
 		return (
-			<Prompt message="You have unsaved changes." when={hasUnsavedChanges} />
+			<RoutePrompt onRoute={this.onRoute} when={hasUnsavedChanges} />
 		);
 	}
 }
