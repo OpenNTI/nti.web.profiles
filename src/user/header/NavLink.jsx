0435e1bce4a2727ab4f10995434d723a05f6a7a1
Andrew Ligon <andrew.ligon@nextthought.com>
2018-10-18 10:12:21 -0500

    NTI-6841 add an active class on the trigger is when the link is active

--- src/user/header/NavLink.jsx
+++ src/user/header/NavLink.jsx
@@ -5,7 +5,9 @@ import {LinkTo} from '@nti/web-routing';
 export default class ProfileHeaderNavLink extends React.Component {
 	static propTypes = {
 		title: PropTypes.string,
-		onDismiss: PropTypes.func
+		onDismiss: PropTypes.func,
+		addTriggerClass: PropTypes.func,
+		removeTriggerClass: PropTypes.func
 	}
 
 	onClick = () => {
@@ -16,11 +18,35 @@ export default class ProfileHeaderNavLink extends React.Component {
 		}
 	}
 
+	onActivate = () => {
+		const {addTriggerClass} = this.props;
+
+		if (addTriggerClass) {
+			addTriggerClass('active');
+		}
+	}
+
+	onDeactivate = () => {
+		const {removeTriggerClass} = this.props;
+
+		if (removeTriggerClass) {
+			removeTriggerClass('active');
+		}
+	}
+
 	render () {
 		const {title} = this.props;
 
 		return (
-			<LinkTo.Object {...this.props} className="profile-header-nav-link" onClick={this.onClick} data-title={title} activeClassName="active">
+			<LinkTo.Object
+				{...this.props}
+				className="profile-header-nav-link"
+				onClick={this.onClick}
+				data-title={title}
+				activeClassName="active"
+				onActivate={this.onActivate}
+				onDeactivate={this.onDeactivate}
+			>
 				{title}
 			</LinkTo.Object>
 		);
