1386a049eb9903ad85da77c152c0ccc55121950e
Ray Hatfield <ray.hatfield@nextthought.com>
2018-10-19 16:55:03 -0500

    improve preflight messaging; more tests

--- src/user/edit/messages/factories/server/__test__/factory.spec.js
+++ src/user/edit/messages/factories/server/__test__/factory.spec.js
@@ -1,15 +1,33 @@
 /* eslint-env jest */
+import {getLocale, registerTranslations} from '@nti/lib-locale';
 
 import getMessages from '../factory';
 
 const INVALID_START_YEAR = 'Invalid position start year.';
 const INVALID_END_YEAR = 'Invalid position end year.';
 const GENERIC_INVALID_VALUE_SINGULAR = 'Unable to save. Your submission included an invalid value.';
-const GENERIC_INVALID_VALUE_PLURAL = 'Unable to save. Your submission included an invalid values.';
+const GENERIC_INVALID_VALUE_PLURAL = 'Unable to save. Your submission included invalid values.';
+
+const localeStrings = {
+	'nti-profile-edit': {
+		'server-error-messages': {
+			RequiredMissing: {
+				one: '---The %(fields)s field is required.---',
+				other: '---These fields are required: %(fields)s---'
+			},
+			UnspecifiedValidationError: {
+				one: GENERIC_INVALID_VALUE_SINGULAR,
+				other: GENERIC_INVALID_VALUE_PLURAL
+			}
+		}
+	}
+};
+
+registerTranslations(getLocale(), localeStrings);
 
 
-/* eslint-env jest */
 describe('server message factory test', () => {
+
 	test('Test single validation error', () => {
 		const error = {
 			ValidationErrors: [
@@ -23,7 +41,7 @@ describe('server message factory test', () => {
 		const results = getMessages(error);
 
 		expect(results.length).toEqual(1);
-		expect(results[0]).toEqual(INVALID_START_YEAR);
+		expect(results[0]).toEqual(GENERIC_INVALID_VALUE_SINGULAR);
 	});
 
 	test('Test multiple validation errors', () => {
@@ -42,9 +60,8 @@ describe('server message factory test', () => {
 
 		const results = getMessages(error);
 
-		expect(results.length).toEqual(2);
-		expect(results[0]).toEqual(INVALID_START_YEAR);
-		expect(results[1]).toEqual(INVALID_END_YEAR);
+		expect(results).toHaveLength(1);
+		expect(results[0]).toEqual(GENERIC_INVALID_VALUE_PLURAL);
 	});
 
 	test('Test single validation error, no messages', () => {
@@ -80,4 +97,36 @@ describe('server message factory test', () => {
 		expect(results[0]).toEqual(GENERIC_INVALID_VALUE_PLURAL);
 	});
 
+	test('Test multiple required missing validation errors', () => {
+		const error = {
+			ValidationErrors: [
+				{
+					code: 'RequiredMissing',
+					field: 'school'
+				},
+				{
+					code: 'RequiredMissing',
+					field: 'name'
+				}
+			],
+		};
+
+		const results = getMessages(error);
+
+		expect(results.length).toEqual(1);
+		expect(results[0]).toEqual('---These fields are required: school, name---');
+	});
+
+	test('error is itself a validation error', () => {
+		const field = 'school';
+		const error = {
+			statusCode: 422,
+			code: 'RequiredMissing',
+			field
+		};
+
+		const messages = getMessages(error);
+		expect(messages).toHaveLength(1);
+		expect(messages[0]).toEqual(`---The ${field} field is required.---`);
+	});
 });
--- src/user/edit/messages/factories/server/factory.js
+++ src/user/edit/messages/factories/server/factory.js
@@ -3,11 +3,16 @@ import Logger from '@nti/util-logger';
 
 import {ensureArray as arr} from '../../../../../util';
 
+const noop = () => void 0;
+
 const logger = Logger.get('nti-profiles:edit:messages:server');
 
 const CODE_REQUIRED = 'RequiredMissing';
 const CODE_INVALID = 'ConstraintNotSatisfied';
+const BOTH = 'RequiredAndInvalid';
 const UNSPECIFIED_VALIDATION_ERROR = 'UnspecifiedValidationError';
+const VALIDATION_ERROR = 'ValidationError';
+const UNKNOWN_ERROR = 'UnknownError';
 const LIST_DELIMITER = 'ListDelimiter';
 
 const t = scoped('nti-profile-edit.server-error-messages', {
@@ -21,10 +26,12 @@ const t = scoped('nti-profile-edit.server-error-messages', {
 		one: 'The %(fields)s field is invalid.',
 		other: 'These fields are invalid: %(fields)s'
 	},
+	[BOTH]: 'These fields are required or invalid: %(fields)s',
 	[UNSPECIFIED_VALIDATION_ERROR]: {
 		one: 'Unable to save. Your submission included an invalid value.',
 		other: 'Unable to save. Your submission included invalid values.'
 	},
+	[UNKNOWN_ERROR]: 'Unable to save. An unknown error occurred.',
 	[LIST_DELIMITER]: ', '
 });
 
@@ -32,44 +39,65 @@ const localizeFieldName = name => !name ? null : t(['fieldNames', name], {fallba
 
 const extractValidationErrors = x => {
 	// has ValidationErrors or is a validation error or bail
-	return arr(x.ValidationErrors || (x.statusCode === 422 && x.field) ? x : null).filter(Boolean);
+	return arr(x.ValidationErrors || ((x.statusCode === 422 && x.field) ? x : null)).filter(Boolean);
 };
 
-const reduceMessages = (acc, error) => {
+const categorize = errors => arr(errors).reduce((acc, error) => {
 	const validation = extractValidationErrors(error);
-
 	if (validation.length > 0) {
-		let messages = [];
+		acc[VALIDATION_ERROR] = [...(acc[VALIDATION_ERROR] || []), ...validation];
+	}
+	else {
+		acc[UNKNOWN_ERROR] = [...(acc[UNKNOWN_ERROR] || []), error];
+	}
+	return acc;
+}, {});
 
+const handlers = {
+	[VALIDATION_ERROR]: errors => {
 		// extract an array of field names (localized)
-		const fields = validation.map(({field}) => localizeFieldName(field)).filter(Boolean);
+		const fields = errors.map(({field}) => localizeFieldName(field)).filter(Boolean);
 
 		// bail with a generic error message if we can't get the field names.
 		if (fields.length === 0) { // validation errors with unidentifiable fields?
-			const messagesFromValidationErrors = validation.map(e => e.message).filter(Boolean);
-
-			if(messagesFromValidationErrors.length > 0) {
-				return acc.concat(validation.map(e => e.message));
-			}
-
 			logger.warn('Unable to identify fields in ValidationErrors. Using generic validation message.');
-			return acc.concat([t(UNSPECIFIED_VALIDATION_ERROR, {count: validation.length})]);
+			return t(UNSPECIFIED_VALIDATION_ERROR, {count: errors.length});
 		}
 
-		const key = validation.every(e => e.code === CODE_REQUIRED) ? CODE_REQUIRED : CODE_INVALID;
+		const types = errors.reduce((result, {code} = {}) => {
+			result.add(code === CODE_REQUIRED ? CODE_REQUIRED : CODE_INVALID);
+			return result;
+		}, new Set());
 
-		messages.push(t(key, {
+		const key = types.size > 1
+			? BOTH
+			: types.has(CODE_REQUIRED)
+				? CODE_REQUIRED
+				: CODE_INVALID;
+
+		return t(key, {
 			count: fields.length,
 			fields: fields.join(t(LIST_DELIMITER, {fallback: ', '}))
-		}));
+		});
+	},
 
-		return acc.concat(messages);
+	[UNKNOWN_ERROR]: errors => {
+		return errors.map(e => {
+			const {Message, message = Message, code} = e;
+			return message || (code && !t.isMissing(code) && t(code)) || t('unknown');
+		});
 	}
-
-	const {Message, message = Message, code} = error;
-	return acc.concat([message || (code && !t.isMissing(code) && t(code)) || t('unknown')]);
 };
 
+
 export default function getMessages (errors) {
-	return arr(errors).reduce(reduceMessages, []);
+	// validation error or unknown
+	const byType = categorize(errors);
+
+	return Object.entries(byType)
+		.map(([type, errs]) => (
+			arr((handlers[type] || noop)(errs))
+		))
+		.reduce((acc, chunk) => [...acc, ...chunk], [])
+		.filter(Boolean);
 }
