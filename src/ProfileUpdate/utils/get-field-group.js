1d130995c00e9eb070b74af864a52ae1e0c403a2
Andrew Ligon <andrew.ligon@nextthought.com>
2019-03-20 16:20:25 -0500

    NTI-7532 NTI-7595 only show optional fields if their required counterpart has an error

--- src/ProfileUpdate/utils/get-field-group.js
+++ src/ProfileUpdate/utils/get-field-group.js
@@ -5,6 +5,10 @@ function getField (schema, error) {
 	};
 }
 
+function hasError (name, errors) {
+	return errors.some(error => error.field === name);
+}
+
 const DEFAULT = Symbol('Default');
 
 const TYPE_HANDLERS = {
@@ -14,23 +18,38 @@ const TYPE_HANDLERS = {
 		];
 	},
 	'ISALLTUserProfile': (schema, validationErrors) => {
-		return [
-			[
-				...validationErrors.map(x => getField(schema[x.field], x)),
-				getField(schema['myers_briggs_response'], null),
-				getField(schema['giant_5_voices_response'], null),
-				getField(schema['five_q_response'], null)
-			]
+		const fields = [
+			...validationErrors.map(x => getField(schema[x.field], x))
 		];
+
+		if (hasError('myers_briggs', validationErrors)) {
+			fields.push(getField(schema['myers_briggs_response'], null));
+		}
+
+		if (hasError('giant_5_voices', validationErrors)) {
+			fields.push(getField(schema['giant_5_voices_response'], null));
+		}
+
+		if (hasError('five_q', validationErrors)) {
+			fields.push(getField(schema['five_q_response'], null));
+		}
+
+		return [fields];
 	},
 	'IOPSRCUserProfile': (schema, validationErrors) => {
-		return [
-			[
-				...validationErrors.map(x => getField(schema[x.field], x)),
-				getField(schema['admin_district_names'], null),
-				getField(schema['teacher_certification_number'], null)
-			]
+		const fields = [
+			...validationErrors.map(x => getField(schema[x.field], x))
 		];
+
+		if (hasError('is_district_admin', validationErrors)) {
+			fields.push(getField(schema['admin_district_names'], null));
+		}
+
+		if (hasError('teacher_certification', validationErrors)) {
+			fields.push(getField(schema['teacher_certification_number'], null));
+		}
+
+		return [fields];
 	}
 };
 
