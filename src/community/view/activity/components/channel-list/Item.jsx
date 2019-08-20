5dd169f504e5fa2b06d4651f171c55450fad06b9
Andrew Ligon <andrew.ligon@nextthought.com>
2019-08-20 13:47:06 -0500

    show all activity for the all activity channel

--- src/community/view/activity/components/channel-list/Item.jsx
+++ src/community/view/activity/components/channel-list/Item.jsx
@@ -1,22 +1,27 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import classnames from 'classnames/bind';
+import {scoped} from '@nti/lib-locale';
 import {LinkTo} from '@nti/web-routing';
 
 import Styles from './Styles.css';
 
 const cx = classnames.bind(Styles);
+const t = scoped('nti-profiles.community.view.activity.components.channel-list.Item', {
+	allActivity: 'All Activity'
+});
 
 ChannelListItem.propTypes = {
 	channel: PropTypes.shape({
-		title: PropTypes.string
+		title: PropTypes.string,
+		isAllActivityChannel: PropTypes.bool
 	}),
 	active: PropTypes.bool
 };
 export default function ChannelListItem ({channel, active}) {
 	return (
 		<LinkTo.Object object={channel} className={cx('channel', {active})} data-title={channel.title}>
-			{channel.title}
+			{channel.isAllActivityChannel ? t('allActivity') : channel.title}
 		</LinkTo.Object>
 	);
 }
