f834d78937183229ecc06dc73d1a7e046983c68a
Andrew Ligon <andrew.ligon@nextthought.com>
2019-08-28 11:08:04 -0500

    NTI-8348 show the channel report if there is one

--- src/community/view/activity/components/channel-list/Item.jsx
+++ src/community/view/activity/components/channel-list/Item.jsx
@@ -1,27 +1,29 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import classnames from 'classnames/bind';
-import {scoped} from '@nti/lib-locale';
 import {LinkTo} from '@nti/web-routing';
+import {Text} from '@nti/web-commons';
+import {Launch} from '@nti/web-reports';
 
 import Styles from './Styles.css';
 
 const cx = classnames.bind(Styles);
-const t = scoped('nti-profiles.community.view.activity.components.channel-list.Item', {
-	allActivity: 'All Activity'
-});
 
 ChannelListItem.propTypes = {
 	channel: PropTypes.shape({
 		title: PropTypes.string,
-		isAllActivityChannel: PropTypes.bool
+		isAllActivityChannel: PropTypes.bool,
+		Reports: PropTypes.array
 	}),
 	active: PropTypes.bool
 };
 export default function ChannelListItem ({channel, active}) {
 	return (
 		<LinkTo.Object object={channel} className={cx('channel', {active})} data-title={channel.title}>
-			{channel.isAllActivityChannel ? t('allActivity') : channel.title}
+			<Text.Base className={cx('channel-label')}>
+				{channel.title}
+			</Text.Base>
+			<Launch.Button report={channel.Reports && channel.Reports[0]} className={cx('channel-report')} />
 		</LinkTo.Object>
 	);
 }
