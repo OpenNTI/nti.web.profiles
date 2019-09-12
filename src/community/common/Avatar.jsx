2ff684825c1def471dd9ebe522af17e60d5c26a8
Andrew Ligon <andrew.ligon@nextthought.com>
2019-09-12 11:28:50 -0500

    add alt text to the avatar and prevent the layout from jumping when it loads

--- src/community/common/Avatar.jsx
+++ src/community/common/Avatar.jsx
@@ -1,25 +1,43 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import {scoped} from '@nti/lib-locale';
 import {Image} from '@nti/web-commons';
 
 import DefaultCommunityAvatar from './assets/default-community-avatar.png';
 
+const aspectRatio = 334 / 175;
 const DefaultAvatar = typeof DefaultCommunityAvatar !== 'string' ? DefaultCommunityAvatar.src : DefaultCommunityAvatar;
+const t = scoped('nti-profiles.community.common.Avatar', {
+	alt: '%(displayName)s Avatar'
+});
 
+
+CommunityAvatar.aspectRatio = aspectRatio;
 CommunityAvatar.Default = DefaultAvatar;
 CommunityAvatar.hasAvatar = (community) => community && !community.noAvatar;
 CommunityAvatar.propTypes = {
+	className: PropTypes.string,
 	community: PropTypes.shape({
 		noAvatar: PropTypes.bool,
-		avatarURL: PropTypes.string
+		avatarURL: PropTypes.string,
+		displayName: PropTypes.string
 	})
 };
-export default function CommunityAvatar ({community, ...otherProps}) {
+export default function CommunityAvatar ({community, className, ...otherProps}) {
 	if (!community || community.noAvatar) { return null; }
 
-	const {avatarURL} = community;
+	const {avatarURL, displayName} = community;
 
 	return (
-		<Image src={avatarURL} fallback={DefaultAvatar} {...otherProps} />
+		<Image.Container aspectRatio={aspectRatio} className={className}>
+			<Image
+				src={avatarURL}
+				fallback={DefaultAvatar}
+				alt={t('alt', {displayName})}
+				aspectRatio={aspectRatio}
+				letterbox="black"
+				{...otherProps}
+			/>
+		</Image.Container>
 	);
 }
