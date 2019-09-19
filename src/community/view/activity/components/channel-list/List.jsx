d2d78d164ccb87319d61945318b156f063419c37
Andrew Ligon <andrew.ligon@nextthought.com>
2019-09-19 17:18:53 -0500

    NTI-8470 trust the channel's list order

--- src/community/view/activity/components/channel-list/List.jsx
+++ src/community/view/activity/components/channel-list/List.jsx
@@ -19,24 +19,6 @@ export default function ChannelList ({list, activeChannel}) {
 	const {label, channels} = list;
 	const activeId = activeChannel && activeChannel.getID();
 
-	const {pinned, sortable} = channels.reduce((acc, channel) => {
-		if (channel.pinned) {
-			acc.pinned.push(channel);
-		} else {
-			acc.sortable.push(channel);
-		}
-
-		return acc;
-	}, {pinned: [], sortable: []});
-	
-	const renderChannel = (channel) => {
-		return (
-			<li key={channel.getID()}>
-				<Item channel={channel} active={activeId === channel.getID()} />
-			</li>
-		);
-	};
-
 	return (
 		<div className={cx('channel-list', {'has-label': !!label})}>
 			{label && (
@@ -45,8 +27,13 @@ export default function ChannelList ({list, activeChannel}) {
 				</Text.Base>
 			)}
 			<ul>
-				{pinned.map(renderChannel)}
-				{sortable.map(renderChannel)}
+				{channels.map((channel) => {
+					return (
+						<li key={channel.getID()}>
+							<Item channel={channel} active={activeId === channel.getID()} />
+						</li>
+					);
+				})}
 			</ul>
 		</div>
 	);
