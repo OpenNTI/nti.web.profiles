5c8dca48d949401b27ecf9429f0b0755837a1701
Andrew Ligon <andrew.ligon@nextthought.com>
2019-12-12 16:04:15 -0600

    NTI-8750 use the shared asset editor for community assets

--- src/community/asset-editor/View.jsx
+++ src/community/asset-editor/View.jsx
@@ -1,71 +1,72 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import classnames from 'classnames/bind';
-import {Loading} from '@nti/web-commons';
+import {AssetEditor} from '@nti/web-whiteboard';
 
-import Store from './Store';
-import Style from './View.css';
-import Controls from './components/Controls';
-import EditorBody from './components/EditorBody';
-import TypeSwitcher from './components/TypeSwitcher';
+import {Avatar, Background} from '../common';
 
-const cx = classnames.bind(Style);
+const DefaultAssets = {
+	avatarURL: Avatar.Default,
+	backgroundURL: Background.Default
+};
 
-export default
-@Store.connect(['loading', 'saving', 'save'])
-class CommunityAssetEditor extends React.Component {
-	static syncAssets = Store.syncAssets
+const Config = {
+	avatarURL: {
+		imageEditor: {crop: {aspectRatio: Avatar.aspectRatio}},
+		solidEditor: {aspectRatio: Avatar.aspectRatio},
+		gradientEditor: {aspectRatio: Avatar.aspectRatio}
+	},
+	backgroundURL: {
+		imageEditor: {
+			crop: {aspectRatio: 3 / 2},
+			blur: {radius: 50, minBlur: 0, maxBlur: 50},
+			darken: {color: '#000', opacity: 0.3}
+		},
+		solidEditor: { aspectRatio: 3 / 2 },
+		gradientEditor: { aspectRatio: 3 / 2 }
+	}
+};
 
-	static deriveBindingFromProps (props) {
-		return {
-			community: props.community,
-			assetName: props.assetName,
-			afterSave: props.afterSave,
-			onCancel: props.onCancel
+CommunityAssetEditor.syncAssets = (community) => community.save({backgroundURL: null});
+CommunityAssetEditor.propTypes = {
+	community: PropTypes.shape({
+		avatarURL: PropTypes.string,
+		backgroundURL: PropTypes.string,
+		putToLink: PropTypes.func,
+		refresh: PropTypes.func
+	}),
+	assetName: PropTypes.oneOf(['avatarURL', 'backgroundURL']),
+	afterSave: PropTypes.func,
+	onCancel: PropTypes.func
+};
+export default function CommunityAssetEditor ({community, assetName, afterSave, onCancel}) {
+	const asset = community[assetName] || DefaultAssets[assetName];
+	const config = Config[assetName];
+
+	const onSave = async (file) => {
+		const reader = new FileReader();
+
+		const dataURL = await new Promise ((fulfill, reject) => {
+			reader.onerror = reject;
+			reader.onload = () => fulfill(reader.result);
+
+			reader.readAsDataURL(file);
+		});
+
+		const data = {
+			[assetName]: dataURL
 		};
-	}
 
-	static propTypes = {
-		community: PropTypes.shape({
-			avatarURL: PropTypes.string,
-			backgroundURL: PropTypes.string
-		}),
-		assetName: PropTypes.string,
-		afterSave: PropTypes.func,
-		onCancel: PropTypes.func,
+		const resp = await community.putToLink('edit', data);
+		await community.refresh(resp);
 
-		loading: PropTypes.bool,
-		saving: PropTypes.bool,
-		save: PropTypes.func
-	}
+		afterSave();
+	};
 
-	onSubmit = (e) => {
-		e.preventDefault();
-		e.stopPropagation();
-
-		const {save} = this.props;
-
-		if (save) {
-			save();
-		}
-	}
-
-
-	render () {
-		const {loading} = this.props;
-
-		return (
-			<div className={cx('community-asset-editor')}>
-				<Loading.Placeholder loading={loading} fallback={(<Loading.Spinner />)}>
-					<div className={cx('navigation')}>
-						<TypeSwitcher />
-					</div>
-					<form className={cx('body')} onSubmit={this.onSubmit}>
-						<EditorBody />
-						<Controls />
-					</form>
-				</Loading.Placeholder>
-			</div>
-		);
-	}
+	return (
+		<AssetEditor asset={asset} onSave={onSave} onCancel={onCancel}>
+			<AssetEditor.Image format={config.imageEditor} />
+			<AssetEditor.SolidColor format={config.solidEditor} />
+			<AssetEditor.LinearGradient format={config.gradientEditor} />
+		</AssetEditor>
+	);
 }
