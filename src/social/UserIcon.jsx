2a086d0f090d8d6d173c64d72f0bedaac981c010
Mahmoud Mousa Hamad <mahmoudmousahamad@gmail.com>
2021-01-08 18:50:39 +0200

    Add UserIcon with presence and unread badges

---
+++ src/social/UserIcon.jsx
@@ -0,0 +1,32 @@
+import PropTypes from 'prop-types';
+import { Avatar, Badge } from '@nti/web-commons';
+
+UserIcon.propTypes = {
+	user: PropTypes.object.isRequired,
+	count: PropTypes.number.isRequired,
+};
+
+const Icon = styled('div')`
+	--size: 42px;
+	vertical-align: middle;
+	border-radius: 50%;
+	max-width: var(--size);
+
+	& img,
+	& svg {
+		width: var(--size);
+		height: var(--size);
+	}
+`;
+
+export default function UserIcon ( { user, count } ) {
+	return (
+		<Badge theme={Badge.THEMES.SUCCESS} position={Badge.POSITIONS.BOTTOM_RIGHT}>
+			<Badge badge={count} position={Badge.POSITIONS.TOP_LEFT}>
+				<Icon>
+					<Avatar entity={user} />
+				</Icon>
+			</Badge>
+		</Badge>
+	);
+}
