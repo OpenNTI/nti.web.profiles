d06e21ac32e585254d076e6776fcb6fb499e4795
Mahmoud Mousa Hamad <mahmoudmousahamad@gmail.com>
2021-01-12 18:27:04 +0200

    Implement load

--- src/social/Store.js
+++ src/social/Store.js
@@ -19,8 +19,28 @@ export default class Store extends Stores.SimpleStore {
 		// TODO: this method should update the list of active users as they change their presence
 	}
 
-	async onPresenceChanged (user, presenceInfo) {
+	async load () {
+		if (this.initialLoad) return;
+		this.initialLoad = true;
 
+		// Subscribe to incoming messages
+		subscribeToIncomingMessage((sender, message) => this.onIncomingMessage(sender, message));
+
+		// Subscribe to presence change
+		subscribeToPresenceChange((user, presenceInfo) => this.onPresenceChanged(user, presenceInfo));
+
+		this.set({Loading: true});
+
+		try {
+			await this.resolveChat();
+			this.updateUnread();
+
+		} catch (e) {
+			this.set({
+				Loading: false,
+				Error: e,
+			});
+		}
 	}
 
 	async load () {
