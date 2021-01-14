40dc5ec0181b1f4c564efa455cbfdddc16ba82c8
Mahmoud Mousa Hamad <mahmoudmousahamad@gmail.com>
2021-01-15 01:44:45 +0200

    Edit styles, add presence icon

--- src/social/UserIcon.jsx
+++ src/social/UserIcon.jsx
@@ -10,17 +10,49 @@ UserIcon.propTypes = {
 
 const Icon = styled("div")`
 	--size: 42px;
-	vertical-align: middle;
-	border-radius: 50%;
-	max-width: var(--size);
-	position: absolute;
-	left: 0;
-	top: 0;
 
 	& img,
 	& svg {
 		width: var(--size);
 		height: var(--size);
+		border-radius: 50%;
+	}
+`;
+
+const PresenceIcon = styled("div")`
+	position: absolute;
+	right: 0;
+	bottom: 0;
+	width: 11px;
+	height: 11px;
+	background-color: white;
+	border-radius: 11px;
+	z-index: 3;
+
+	&:after {
+		background: var(--primary-green);
+		width: 7px;
+		height: 7px;
+		border-radius: 90px;
+		content: "";
+		position: absolute;
+		display: block;
+		left: 2px;
+		top: 2px;
+	}
+`;
+
+const styles = css`
+	.container {
+		margin: 8px auto;
+		width: 42px;
+		height: 42px;
+		cursor: pointer;
+	}
+
+	.profile {
+		position: relative;
+		height: -webkit-fill-available;
 	}
 `;
 
@@ -28,15 +60,18 @@ export default function UserIcon ( { user } ) {
 	const {
 		unreadCount,
 		updateLastViewed,
-	} = Store.useValues();
+	} = Store.useValue();
 
 	return (
-		<Badge theme={Badge.THEMES.SUCCESS} position={Badge.POSITIONS.BOTTOM_RIGHT}>
-			<Badge badge={unreadCount[user.id]} position={Badge.POSITIONS.TOP_LEFT}>
+		<div className={styles.container}>
+			<div className={styles.profile}>
 				<Icon onClick={updateLastViewed(user)}>
-					<Avatar entity={user} />
+					<Badge badge={unreadCount[user.id]} position={Badge.POSITIONS.TOP_LEFT} {...Badge.offset(0, 4)}>
+						<Avatar entity={user} />
+					</Badge>
+					<PresenceIcon />
 				</Icon>
-			</Badge>
-		</Badge>
+			</div>
+		</div>
 	);
 }
