ebbe7606c08bdf922517b080e4dda6e4e73c667d
Mahmoud Mousa Hamad <mahmoudmousahamad@gmail.com>
2021-01-14 18:37:24 +0200

    Add the master chat panel component

---
+++ src/social/View.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import {DateIcon} from '@nti/web-calendar';
+
+import ChatIcon from './Icon';
+import Store from './Store';
+import VerticalPanel from './Panel';
+import useWindowSize from './utils/useWindowSize';
+
+export default function ChatPanel () {
+	const {
+		load
+	} = Store.useValues();
+
+	useEffect(() => {
+		load();
+	}, [load]);
+
+	const windowSize = useWindowSize();
+
+	const {visible, setVisible} = useState(false);
+	const {expanded, setExpanded} = useState(false);
+
+	return (
+		<ChatIcon onClick={setVisible(!visible)}>
+			{(visible || windowSize > 1200) && (
+				<VerticalPanel expanded={expanded} setExpanded={setExpanded(!expanded)}>
+					<DateIcon />
+				</VerticalPanel>
+			)}
+		</ChatIcon>
+	);
+}
