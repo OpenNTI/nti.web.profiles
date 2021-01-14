df08749cadc46ebf7566ee51b03f85c8dc8718f9
Mahmoud Mousa Hamad <mahmoudmousahamad@gmail.com>
2021-01-15 01:47:41 +0200

    Add styled div and edit styles

--- src/social/expanded/Contacts.jsx
+++ src/social/expanded/Contacts.jsx
@@ -1,7 +1,35 @@
+import React from 'react';
+
 import EntryContainer from './EntryContainer';
 
 const ContactsIcon = styled.div`
-		background: url('./assets/contacts.png');
+	background: url("../assets/contacts.png");
+	overflow: hidden;
+	top: 1px;
+	left: 1px;
+	right: auto;
+	width: 42px;
+	height: 42px;
+	position: absolute;
+`;
+
+const ContactsText = styled('div')`
+	margin-left: 48px;
+	position: relative;
+	width: 150px;
+	height: 42px;
+	color: #fff;
+	padding: 12px 4px 0 4px;
+	overflow: hidden;
+	font-size: 14px;
+	font-weight: 200;
+	white-space: nowrap;
+	overflow: hidden;
+	text-overflow: ellipsis;
+
+	&:after {
+		content: 'Contacts'
+	}
 `;
 
 export default function ContactsButton () {
@@ -9,6 +37,7 @@ export default function ContactsButton () {
 	return (
 		<EntryContainer>
 			<ContactsIcon />
+			<ContactsText />
 		</EntryContainer>
 	);
 }
