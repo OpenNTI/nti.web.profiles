f5ba4d8aae70cf3f3e7a74fab696ffb5c40b41d2
Mahmoud Mousa Hamad <mahmoudmousahamad@gmail.com>
2021-01-14 18:36:29 +0200

    Add ChatIcon component

--- src/social/Icon.jsx
+++ src/social/Icon.jsx
@@ -1,5 +1,44 @@
+import { Theme } from '@nti/web-commons';
+import PropTypes from 'prop-types';
 import React from 'react';
 
-export default function ChatIcon () {
 
+const styles = css`
+	.container {
+		padding: 23px;
+	}
+
+	.icon {
+		&::after {
+			content: "";
+			display: block;
+			background-position: center;
+			width: 21px;
+			height: 24px;
+			opacity: 1;
+			background-image: url("./assets/icon-dark.svg");
+		}
+
+		&:focus {
+			outline: 0;
+		}
+	}
+
+	.light::after {
+		background-image: url("./assets/icon-white.svg");
+	}
+`;
+
+ChatIcon.propTypes = {
+	onClick: PropTypes.func.isRequired,
+};
+
+export default function ChatIcon ( { onClick } ) {
+	const theme = Theme.useThemeProperty('icon');
+
+	return (
+		<div className={styles.container} ref={ref}>
+			<div className={cx(styles.icon, styles[theme])} onClick={onClick}/>
+		</div>
+	);
 }
