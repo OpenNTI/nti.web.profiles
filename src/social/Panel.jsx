bd31fdd769ca101f74a1090cb202c22a154609a4
Mahmoud Mousa Hamad <mahmoudmousahamad@gmail.com>
2021-01-14 18:36:39 +0200

    Add panel component

--- src/social/Panel.jsx
+++ src/social/Panel.jsx
@@ -1,5 +1,20 @@
+import PropTypes from 'prop-types';
 import React from 'react';
 
-export default function Panel () {
+import ExpandedPanel from './expanded';
+import CollapsedPanel from './collapsed';
 
+VerticalPanel.propTypes = {
+	expanded: PropTypes.bool.isRequired,
+	setExpanded: PropTypes.func.isRequired,
+};
+
+export default function VerticalPanel ( { expanded, children, setExpanded } ) {
+	return (
+		expanded ? (
+			<ExpandedPanel collapse={setExpanded(false)} />
+		) : (
+			<CollapsedPanel expand={setExpanded(true)}>{children}</CollapsedPanel>
+		)
+	);
 }
