7fd6708b2eb1018753949651d0a146357c1c36fa
Mahmoud Mousa Hamad <mahmoudmousahamad@gmail.com>
2021-01-15 22:07:45 +0200

    macro styles into styled components

--- src/social/BadgedAvatar.jsx
+++ src/social/BadgedAvatar.jsx
@@ -3,46 +3,7 @@ import PropTypes from 'prop-types';
 import { Avatar, Badge } from '@nti/web-commons';
 
 import Store from './Store';
-
-const Icon = styled('div')`
-	--size: 42px;
-
-	position: relative;
-	margin: 8px auto;
-	width: 42px;
-	height: 42px;
-	cursor: pointer;
-
-	& img,
-	& svg {
-		width: var(--size);
-		height: var(--size);
-		border-radius: 50%;
-	}
-`;
-
-const PresenceIcon = styled('div')`
-	position: absolute;
-	right: 0;
-	bottom: 0;
-	width: 11px;
-	height: 11px;
-	background-color: white;
-	border-radius: 11px;
-	z-index: 3;
-
-	&:after {
-		background: var(--primary-green);
-		width: 7px;
-		height: 7px;
-		border-radius: 90px;
-		content: "";
-		position: absolute;
-		display: block;
-		left: 2px;
-		top: 2px;
-	}
-`;
+import { AvatarContainer, PresenceCircle } from './parts';
 
 BadgedAvatar.propTypes = {
 	entity: PropTypes.oneOfType([
@@ -58,11 +19,11 @@ export default function BadgedAvatar ( { entity } ) {
 	} = Store.useValue();
 
 	return (
-		<Icon onClick={updateLastViewed(entity)}>
+		<AvatarContainer onClick={updateLastViewed(entity)}>
 			<Badge badge={unreadCount[entity.id]} position={Badge.POSITIONS.TOP_LEFT} {...Badge.offset(0, 4)}>
 				<Avatar entity={entity} />
 			</Badge>
-			<PresenceIcon />
-		</Icon>
+			<PresenceCircle />
+		</AvatarContainer>
 	);
 }
