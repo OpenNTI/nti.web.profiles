21c2c55d08d5030ea7291d0f94388f7c650e5bfb
Mahmoud Mousa Hamad <mahmoudmousahamad@gmail.com>
2021-01-15 02:58:22 +0200

    Add useMatchesMediaQuery hook

--- src/social/View.js
+++ src/social/View.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {DateIcon} from '@nti/web-calendar';
+import {Hooks} from '@nti/web-commons';
 
 import ChatIcon from './Icon';
 import Store from './Store';
@@ -14,15 +15,15 @@ export default function ChatPanel () {
 		load();
 	}, [load]);
 
-	const windowSize = useWindowSize();
+	const matches = Hooks.useMatchesMediaQuery('(min-width: 1200px)');
 
 	const {visible, setVisible} = useState(false);
 	const {expanded, setExpanded} = useState(false);
 
 	return (
-		<ChatIcon onClick={setVisible(!visible)}>
-			{(visible || windowSize > 1200) && (
-				<VerticalPanel expanded={expanded} setExpanded={setExpanded(!expanded)}>
+		<ChatIcon onClick={setVisible(!visible)} visible={!matches}>
+			{(visible || matches) && (
+				<VerticalPanel expanded={expanded} setExpanded={(val) => setExpanded(val)}>
 					<DateIcon />
 				</VerticalPanel>
 			)}
