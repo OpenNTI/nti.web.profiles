a5cfa113249f05dc5bb954b0d1ec1168caa51219
Mahmoud Mousa Hamad <mahmoudmousahamad@gmail.com>
2021-01-20 17:11:30 +0200

    Delete unused stuff

--- src/social/Store.js
+++ src/social/Store.js
@@ -1,12 +1,4 @@
 import { Stores } from '@nti/lib-store';
-import { getService } from '@nti/web-client';
-
-import { subscribeToIncomingMessage, subscribeToPresenceChange } from './Socket';
-
-const MESSAGE_INBOX = 'RUGDByOthersThatIMightBeInterestedIn';
-const CONTENT_ROOT = 'tag:nextthought.com,2011-10:Root';
-
-const BATCH_SIZE = 10;
 
 export default class Store extends Stores.SimpleStore {
 	static Singleton = true;
@@ -55,90 +47,15 @@ export default class Store extends Stores.SimpleStore {
 		if (this.initialLoad) {return;}
 		this.initialLoad = true;
 
-		// Subscribe to incoming messages
-		subscribeToIncomingMessage((sender, message) => this.onIncomingMessage(sender, message));
-
-		// Subscribe to presence change
-		subscribeToPresenceChange((user, presenceInfo) => this.onPresenceChanged(user, presenceInfo));
-
 		this.set({Loading: true});
 
 		try {
 			this.set({activeUsers: this.activeUsers});
-
-			await this.loadConversations();
-
-			this.updateUnread();
-
 		} catch (e) {
 			this.set({
 				Loading: false,
 				Error: e,
 			});
-		}
-	}
-
-	loadConversations = async () => {
-		const service = await getService();
-		const pageInfo = await service.getPageInfo(CONTENT_ROOT);
-		// TODO: check the way to get the url
-		this.url = pageInfo.getLink(MESSAGE_INBOX);
-
-		await Promise.all(
-			this.get('ActiveUsers').map(
-				this.loadConversation
-			)
-		);
-	}
-
-	loadConversation = async (user) => {
-		// TODO: check the method of getting the batch of messages for the user
-		// TODO: check the service.get url
-		const service = await getService();
-
-		const Batches = this.get('Batches') || {}, LastViewed = this.get('LastViewed') || {};
-
-		Batches[user.id] = await service.getBatch(this.url, {
-			batchStart: 0,
-			batchSize: BATCH_SIZE,
-		});
-
-		// TODO: check lastViewed link for the chat
-		LastViewed[user.id] = new Date(parseFloat(await service.get(`${this.url}/${user.id}/lastViewed`), 10) * 1000);
-
-		this.set({ Batches, LastViewed });
-	}
-
-	updateUnread () {
-		const UnreadCount = {};
-
-		const mod = (x) => x.getLastModified() || x.getCreatedTime();
-		const inc = (base, item, lastViewed) => base + (mod(item) > lastViewed ? 1 : 0);
-
-		this.get('ActiveUsers').forEach((user) => {
-			const msgs = this.get('Batches')[user.id];
-
-			const lastViewed = this.get('LastViewed')[user.id];
-
-			UnreadCount[user.id] = msgs.reduce((n, item) => inc(n, item, lastViewed), 0);
-		});
-
-		this.set({UnreadCount});
-	}
-
-	async updateLastViewed (user) {
-		const LastViewed = this.get('LastViewed'), UnreadCount = this.get('UnreadCount');
-
-		// TODO: check lastViewed link for the chat
-		if (this.get('Batches')[user.id].hasLink(`${user.id}/lastViewed`)) {
-			const now = new Date();
-			// TODO: check lastViewed link for the chat
-			this.batch.putToLink(`${user.id}/lastViewed`, now.getTime() / 1000);
-			LastViewed[user.id] = now;
-			UnreadCount[user.id] = 0;
-
-			this.set({LastViewed, UnreadCount});
-			this.updateUnread();
 		}
 	}
 }
