6622c64275e085d507c09dd1d6b0325a67431115
Jonathan Grimes <jsg2021@gmail.com>
2021-01-26 13:48:59 -0600

    to function

--- src/community/view/activity/Frame.jsx
+++ src/community/view/activity/Frame.jsx
@@ -13,53 +13,44 @@ const Queries = {
 	Mobile: ({containerWidth}) => containerWidth < 678
 };
 
-class CommunityActivityFrame extends React.Component {
-	static deriveBindingFromProps (props) {
-		return props.community;
-	}
+CommunityActivityFrame.deriveBindingFromProps = (props) => props.community;
+CommunityActivityFrame.propTypes = {
+	children: PropTypes.element,
+	channelId: PropTypes.string,
+	community: PropTypes.shape({
+		getChannelList: PropTypes.func
+	}),
 
-	static propTypes = {
-		children: PropTypes.element,
-		channelId: PropTypes.string,
-		community: PropTypes.shape({
-			getChannelList: PropTypes.func
-		}),
+	loading: PropTypes.bool,
+	error: PropTypes.any,
+	channels: PropTypes.any
+};
 
-		loading: PropTypes.bool,
-		error: PropTypes.any,
-		channels: PropTypes.any
-	}
+function CommunityActivityFrame (props) {
+	return (
+		<Responsive.Container>
+			<Responsive.Item query={Queries.Desktop} component={Frame} size={Sizes.Desktop} {...props} />
+			<Responsive.Item query={Queries.Tablet} component={Frame} size={Sizes.Tablet} {...props} />
+			<Responsive.Item query={Queries.Mobile} component={Frame} size={Sizes.Mobile} {...props} />
+		</Responsive.Container>
+	);
 
-
-	render () {
-		return (
-			<Responsive.Container>
-				<Responsive.Item query={Queries.Desktop} render={this.renderDesktop} />
-				<Responsive.Item query={Queries.Tablet} render={this.renderTablet} />
-				<Responsive.Item query={Queries.Mobile} render={this.renderMobile} />
-			</Responsive.Container>
-		);
-	}
-
-	renderDesktop = () => this.renderSize(Sizes.Desktop)
-	renderTablet = () => this.renderSize(Sizes.Tablet)
-	renderMobile = () => this.renderSize(Sizes.Mobile)
-
-	renderSize (size) {
-		const {children, loading, error, channels, community, ...otherProps} = this.props;
-
-		if (error) {
-			return (<Error error={error} />);
-		}
-
-		return (
-			<Loading.Placeholder loading={loading || !channels} fallback={<Loading.Spinner large />}>
-				{React.Children.map(children, (child) => {
-					return React.cloneElement(child, {size, channels, community, ...otherProps});
-				})}
-			</Loading.Placeholder>
-		);
-	}
 }
 
 export default Store.connect(['loading', 'channels', 'error'])(CommunityActivityFrame);
+
+
+function Frame ({children, loading, error, channels, community, size, ...props}) {
+
+	if (error) {
+		return (<Error error={error} />);
+	}
+
+	return (
+		<Loading.Placeholder loading={loading || !channels} fallback={<Loading.Spinner large />}>
+			{React.Children.map(children, (child) => {
+				return React.cloneElement(child, {size, channels, community, ...props});
+			})}
+		</Loading.Placeholder>
+	);
+}
