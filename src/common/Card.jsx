e93ba76b1fae12d12cb7f85f322c916ba3262a74
Andrew Ligon <andrew.ligon@nextthought.com>
2021-01-27 17:27:16 -0600

    use less padding on mobile

--- src/common/Card.jsx
+++ src/common/Card.jsx
@@ -2,18 +2,27 @@ import './Card.scss';
 import React from 'react';
 import PropTypes from 'prop-types';
 import cx from 'classnames';
+import {Layouts} from '@nti/web-commons';
+
+const {Responsive} = Layouts;
+
+const ClassList = [
+	{query: ({width}) => width < 1024, className: 'small'}
+];
 
 Card.propTypes = {
 	className: PropTypes.string,
 	title: PropTypes.string,
 	children: PropTypes.any
 };
-
 export default function Card ({className, title, children, ...props}) {
 	return (
-		<section className={cx('user-profile-card', className)}>
+		<Responsive.ClassList
+			className={cx('user-profile-card', className)}
+			classList={ClassList}
+		>
 			{title && <h2>{title}</h2>}
 			{children}
-		</section>
+		</Responsive.ClassList>
 	);
 }
