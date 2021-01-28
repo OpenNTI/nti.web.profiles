f541e727cc0778f3153d150a8bc3ed7018b248d2
Mahmoud Mousa Hamad <mahmoudmousahamad@gmail.com>
2021-01-28 15:49:14 +0200

    Add initial override stuff

--- src/social/BadgedAvatar.jsx
+++ src/social/BadgedAvatar.jsx
@@ -4,26 +4,32 @@ import { Avatar, Badge } from '@nti/web-commons';
 
 import Store from './Store';
 import { AvatarContainer, PresenceCircle } from './parts';
+import ChatWindowView from './ChatWindow';
 
 BadgedAvatar.propTypes = {
 	entity: PropTypes.oneOfType([
 		PropTypes.object,
 		PropTypes.string
 	]).isRequired,
+	presence: PropTypes.string.isRequired,
 };
 
-export default function BadgedAvatar ( { entity } ) {
+export default function BadgedAvatar ( { entity, presence } ) {
 	const {
-		unreadCount,
-		updateLastViewed,
+		unreadCounts,
 	} = Store.useValue();
 
+	const [window, setWindow] = React.useState(false);
+
+	const ChatWindow = ChatWindowView.getChatWindow();
+
 	return (
-		<AvatarContainer onClick={updateLastViewed(entity)}>
-			<Badge badge={unreadCount[entity.id]} position={Badge.POSITIONS.TOP_LEFT} {...Badge.offset(0, 4)}>
+		<AvatarContainer onClick={() => setWindow(!window)}>
+			<Badge badge={unreadCounts ? unreadCounts[entity] : 0} position={Badge.POSITIONS.TOP_LEFT} {...Badge.offset(5, 4)}>
 				<Avatar entity={entity} />
 			</Badge>
-			<PresenceCircle />
+			<PresenceCircle presence={presence}/>
+			<ChatWindow onClose={() => setWindow(false)} entity={entity} visible={window}/>
 		</AvatarContainer>
 	);
 }
