c1f8f1fc71014b6a1858c3f7ddd2089ccfb22c66
Mahmoud Mousa Hamad <mahmoudmousahamad@gmail.com>
2021-02-11 16:10:56 +0200

    Add CalendarWindow override

---
+++ src/social/CalendarWindow.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+let CalendarWindow = null;
+
+CalendarWindowView.setCalendarWindow = (window) => (CalendarWindow = window);
+CalendarWindowView.getCalendarWindow = () => CalendarWindow;
+
+export default function CalendarWindowView () {
+	return (
+		<>
+		</>
+	);
+}
