0f9bbc470e4f80bd38ca9609920227e9f1c09bf4
Jonathan Grimes <jsg2021@gmail.com>
2021-04-16 13:21:42 -0500

    [NTI-10543] activate profile link

--- src/identity/Card.jsx
+++ src/identity/Card.jsx
@@ -1,4 +1,5 @@
 import { scoped } from '@nti/lib-locale';
+import { useAppUser } from '@nti/web-commons';
 import { LinkTo } from '@nti/web-routing';
 
 import { Box, Avatar, Meta, DisplayName, Links, Link } from './Card.parts';
@@ -9,15 +10,16 @@ const t = scoped('session.identity-card', {
 });
 
 export function IdentityCard(props) {
+	const user = useAppUser();
 	return (
 		<Box>
 			<Avatar me />
 			<Meta>
 				<DisplayName me />
 				<Links>
-					<LinkTo.Name as={Link} name="profile">
+					<LinkTo.Object as={Link} object={user}>
 						{t('viewProfile')}
-					</LinkTo.Name>
+					</LinkTo.Object>
 					<LinkTo.Name as={Link} name="account">
 						{t('manageAccount')}
 					</LinkTo.Name>
