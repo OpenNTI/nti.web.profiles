7984d5138527a1851f8e889dd2ecca905501bddb
Andrew Ligon <aligon90@gmail.com>
2021-06-30 13:31:39 -0500

    NTI-10882 expose the tps employee id

--- src/user/about/parts/About.jsx
+++ src/user/about/parts/About.jsx
@@ -6,10 +6,24 @@ import { scoped } from '@nti/lib-locale';
 import { Card, EditorContent } from '../../../common';
 import { LOCALE_PATHS } from '../../constants';
 
+import LabelValueList from './LabelValueList';
+
 const t = scoped(LOCALE_PATHS.ABOUT, {
 	title: 'About',
 	editLabel: 'Tell us about yourself…',
+
+	tpsEmployeeId: 'TPS Employee ID',
 });
+
+const Additional = [
+	user => {
+		if (!user['tps_employee_id']) {
+			return null;
+		}
+
+		return { label: t('tpsEmployeeId'), value: user['tps_employee_id'] };
+	},
+];
 
 export default class About extends React.Component {
 	static propTypes = {
@@ -17,9 +31,10 @@ export default class About extends React.Component {
 	};
 
 	render() {
-		const {
-			user: { about = [] },
-		} = this.props;
+		const { user } = this.props;
+		const { about } = user ?? {};
+
+		const additional = Additional.map(a => a(user)).filter(Boolean);
 
 		return (
 			<Card className="about" title={t('title')}>
@@ -28,6 +43,7 @@ export default class About extends React.Component {
 						<EditorContent content={about} />
 					</div>
 				)}
+				<LabelValueList values={additional} />
 			</Card>
 		);
 	}
