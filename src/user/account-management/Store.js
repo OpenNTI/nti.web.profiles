5cc89769413dcc1f596ee03553350b21c771c947
Mahmoud Mousa Hamad <mahmoudmousahamad@gmail.com>
2021-07-09 17:02:00 -0500

    Update store

--- src/user/account-management/Store.js
+++ src/user/account-management/Store.js
@@ -1,11 +1,30 @@
 import { Stores } from '@nti/lib-store';
 import { getService } from '@nti/web-client';
+import { ImageEditor } from '@nti/web-whiteboard';
 
 export default class AccountStore extends Stores.SimpleStore {
 	static Singleton = true;
 
-	constructor () {
+	constructor() {
 		super();
+		this.load();
+	}
+
+	async saveImage(image) {
+		this.set('loading', true);
+		try {
+			const service = await getService();
+			const user = await service.getAppUser();
+
+			await user.refresh();
+			await user.save({ avatarURL: image.src });
+
+			this.set('image', image);
+
+			this.set('loading', false);
+		} catch (e) {
+			this.set('error', e);
+		}
 	}
 
 	async load() {
@@ -14,10 +33,12 @@ export default class AccountStore extends Stores.SimpleStore {
 		try {
 			const service = await getService();
 			const user = await service.getAppUser();
+			const image = await ImageEditor.getImg(user.avatarURL);
 
-			this.set(user);
+			this.set('user', user);
+			this.set('image', image);
 
-			this.set('loading', false);
+			this.this.set('loading', false);
 		} catch (error) {
 			this.set('error', error);
 
