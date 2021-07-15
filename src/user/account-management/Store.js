f22fb0a694b81731d5b07cdcf42c9d18a52b144d
Mahmoud Mousa Hamad <mahmoudmousahamad@gmail.com>
2021-07-15 17:03:40 -0500

    Remove change password stuff, no need.

--- src/user/account-management/Store.js
+++ src/user/account-management/Store.js
@@ -1,5 +1,5 @@
 import { Stores } from '@nti/lib-store';
-import { getAppUser, getService } from '@nti/web-client';
+import { getService } from '@nti/web-client';
 import { ImageEditor } from '@nti/web-whiteboard';
 
 export default class AccountStore extends Stores.SimpleStore {
@@ -10,41 +10,10 @@ export default class AccountStore extends Stores.SimpleStore {
 		this.load();
 	}
 
-	async handlePassword({ formData, json }) {
-		this.set('loading', true);
-
-		try {
-			if (json?.old === json?.new) {
-				const e = new Error('Old and new passwords must be different');
-				e.field = 'old';
-
-				throw e;
-			}
-
-			if (json?.new !== json?.verify) {
-				const e = new Error('Passwords do not match.');
-				e.field = 'verify';
-
-				throw e;
-			}
-
-			formData?.delete?.('verify');
-
-			const user = await getAppUser();
-			await user.changPassword(json.new, json.old);
-
-			this.set('passwordChanged', true);
-		} catch (e) {
-			this.set('loading', false);
-			this.set('error', e);
-		}
-	}
-
 	async saveImage(image) {
 		this.set('loading', true);
 		try {
-			const service = await getService();
-			const user = await service.getAppUser();
+			const user = this.get('user');
 
 			await user.refresh();
 			await user.save({ avatarURL: image.src });
@@ -67,7 +36,6 @@ export default class AccountStore extends Stores.SimpleStore {
 
 			this.set('user', user);
 			this.set('image', image);
-			this.set('passwordChanged', false);
 
 			this.set('loading', false);
 		} catch (error) {
