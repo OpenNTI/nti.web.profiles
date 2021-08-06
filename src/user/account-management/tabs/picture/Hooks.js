7b245f66100507de4c0cbf1e2cfcadb73fa21c27
Mahmoud Mousa Hamad <mahmoudmousahamad@gmail.com>
2021-08-06 15:12:54 -0500

    Workaround when avatarURL is undefined

--- src/user/account-management/tabs/picture/Hooks.js
+++ src/user/account-management/tabs/picture/Hooks.js
@@ -1,8 +1,23 @@
+import { v4 as uuid } from 'uuid';
+import { useEffect } from 'react';
+
 import { useAsyncValue } from '@nti/web-commons';
 import { ImageEditor } from '@nti/web-whiteboard';
 
 export const useImage = user => {
 	const { avatarURL: uri } = user;
 	const reloadNonce = user;
-	return useAsyncValue(uri, () => ImageEditor.getImg(uri), reloadNonce);
+
+	let uniqueId;
+	useEffect(() => {
+		if (!uri) {
+			uniqueId = uuid();
+		}
+	});
+
+	return useAsyncValue(
+		uri || uniqueId,
+		() => (uri ? ImageEditor.getImg(uri) : Promise.resolve('success')),
+		reloadNonce
+	);
 };
