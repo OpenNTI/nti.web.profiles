1f4f60b7b0972b5380aa5d506f061e26647fc168
Mahmoud Mousa Hamad <mahmoudmousahamad@gmail.com>
2021-08-06 15:13:49 -0500

    Remove loading because it's not needed

--- src/user/account-management/tabs/picture/View.jsx
+++ src/user/account-management/tabs/picture/View.jsx
@@ -1,4 +1,4 @@
-import { Loading, Switch, useAppUser, useReducerState } from '@nti/web-commons';
+import { Switch, useAppUser, useReducerState } from '@nti/web-commons';
 
 import { useImage } from './Hooks';
 import { Edit, Main, Upload } from './views';
@@ -7,25 +7,21 @@ export default function PictureTab() {
 	const user = useAppUser();
 	const image = useImage(user);
 
-	const [{ active, baseImage = image, loading }, setState, reset] =
-		useReducerState({
-			active: 'main',
-			loading: false,
-			baseImage: image,
-		});
+	const [{ active, baseImage = image }, setState, reset] = useReducerState({
+		active: 'main',
+		baseImage: image,
+	});
 
 	const onImageCroppingSave = async croppedImage => {
-		setState({
-			active: 'main',
-			loading: true,
-			baseImage: croppedImage,
-		});
 		try {
 			await user.save({ avatarURL: croppedImage.src });
 		} catch (e) {
 			setState({ error: e });
 		} finally {
-			setState({ loading: false });
+			setState({
+				active: 'main',
+				baseImage: croppedImage,
+			});
 		}
 	};
 
@@ -33,32 +29,27 @@ export default function PictureTab() {
 		setState({ baseImage, active: 'edit' });
 
 	return (
-		<Loading.Placeholder
-			loading={loading}
-			fallback={<Loading.Spinner.Large />}
-		>
-			<Switch.Container active={active}>
-				<Switch.Item
-					name="main"
-					component={Main}
-					onUpload={() => setState({ active: 'upload' })}
-					onEdit={() => setState({ active: 'edit' })}
-				/>
-				<Switch.Item
-					name="edit"
-					component={Edit}
-					onCancel={reset}
-					onSave={onImageCroppingSave}
-					image={baseImage}
-				/>
-				<Switch.Item
-					name="upload"
-					component={Upload}
-					onCancel={reset}
-					onSave={onBaseImageSave}
-					baseImage={baseImage}
-				/>
-			</Switch.Container>
-		</Loading.Placeholder>
+		<Switch.Container active={active}>
+			<Switch.Item
+				name="main"
+				component={Main}
+				onUpload={() => setState({ active: 'upload' })}
+				onEdit={() => setState({ active: 'edit' })}
+			/>
+			<Switch.Item
+				name="edit"
+				component={Edit}
+				onCancel={reset}
+				onSave={onImageCroppingSave}
+				image={baseImage}
+			/>
+			<Switch.Item
+				name="upload"
+				component={Upload}
+				onCancel={reset}
+				onSave={onBaseImageSave}
+				baseImage={baseImage}
+			/>
+		</Switch.Container>
 	);
 }
