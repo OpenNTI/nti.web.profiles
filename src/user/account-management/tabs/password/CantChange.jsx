1a89c1852151493558f39aab71af06fbffabdb24
Mahmoud Mousa Hamad <mahmoudmousahamad@gmail.com>
2021-08-17 12:13:51 -0500

    Add can't change component

---
+++ src/user/account-management/tabs/password/CantChange.jsx
@@ -0,0 +1,37 @@
+import { scoped } from '@nti/lib-locale';
+
+const Container = styled.div`
+	padding: 20px;
+	max-width: 500px;
+`;
+
+const Title = styled.div`
+	color: var(--primary-grey);
+	font-size: 24px;
+	font-weight: 400;
+	font-style: italic;
+`;
+
+const SubText = styled.div`
+	font-size: 14px;
+	font-style: normal;
+	font-weight: 300;
+`;
+
+const t = scoped(
+	'nti.web-profiles.user.account-management.tabs.password.CantChange',
+	{
+		title: 'Resetting your password is not allowed.',
+		subtext:
+			'You are not allowed to change your password at this time.Please contact support to request a password change.',
+	}
+);
+
+export function CantChange() {
+	return (
+		<Container data-testid="password-cant-change">
+			<Title>{t('title')}</Title>
+			<SubText>{t('subtext')}</SubText>
+		</Container>
+	);
+}
