ac51fe37860dd97fffce94eae769d1df93379358
Jonathan Grimes <jsg2021@gmail.com>
2021-08-30 16:34:58 -0500

    reduce to function

--- src/user/header/Header.jsx
+++ src/user/header/Header.jsx
@@ -1,27 +1,18 @@
 import './Header.scss';
 import React from 'react';
-import PropTypes from 'prop-types';
 
 import Buttons from './Buttons';
 import Nav from './Nav';
 import UserInfo from './UserInfo';
 
-export default class Header extends React.Component {
-	static propTypes = {
-		entity: PropTypes.object,
-	};
-
-	render() {
-		const { entity } = this.props;
-
-		return (
-			<>
-				<Buttons entity={entity} />
-				<header className="nti-profile-header">
-					<UserInfo entity={entity} />
-					<Nav entity={entity} />
-				</header>
-			</>
-		);
-	}
+export default function Header({ entity }) {
+	return (
+		<>
+			<Buttons entity={entity} />
+			<header className="nti-profile-header">
+				<UserInfo entity={entity} />
+				<Nav entity={entity} />
+			</header>
+		</>
+	);
 }
