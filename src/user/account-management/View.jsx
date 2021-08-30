f85e7eddabe6b2e1bc3c1caf0a08d294ef5425ef
Jonathan Grimes <jsg2021@gmail.com>
2021-08-30 15:21:16 -0500

    Do not surround Dialogs in Suspense. Do not use local-state to track open/close (unmount when onClose is called)

--- src/user/account-management/View.jsx
+++ src/user/account-management/View.jsx
@@ -1,4 +1,4 @@
-import React, { Suspense, useState } from 'react';
+import React, { Suspense, useCallback } from 'react';
 
 import { Loading, Prompt, Text } from '@nti/web-commons';
 import { scoped } from '@nti/lib-locale';
@@ -19,34 +19,25 @@ const Modal = styled(Prompt.BaseWindow)`
 `;
 
 export const AccountManagement = React.forwardRef(({ onClose }, ref) => {
-	const [prompt, setPrompt] = useState(true);
-
-	const handleClose = () => {
-		if (onClose) {
-			onClose();
-		}
-		setPrompt(false);
-	};
+	const handleClose = useCallback(() => void onClose?.(), [onClose]);
 
 	return (
-		<Suspense fallback={<Loading.Spinner />}>
-			{prompt && (
-				<Prompt.Dialog
-					closeOnMaskClick={false}
-					closeOnEscape={true}
-					onBeforeDismiss={handleClose}
-					tall
-				>
-					<Modal
-						title={<Translate localeKey="title" />}
-						doClose={handleClose}
-						buttons={[]}
-					>
-						<Header />
-						<TabsView />
-					</Modal>
-				</Prompt.Dialog>
-			)}
-		</Suspense>
+		<Prompt.Dialog
+			closeOnMaskClick={false}
+			closeOnEscape={true}
+			onBeforeDismiss={handleClose}
+			tall
+		>
+			<Modal
+				title={<Translate localeKey="title" />}
+				doClose={handleClose}
+				buttons={[]}
+			>
+				<Header />
+				<Suspense fallback={<Loading.Spinner />}>
+					<TabsView />
+				</Suspense>
+			</Modal>
+		</Prompt.Dialog>
 	);
 });
