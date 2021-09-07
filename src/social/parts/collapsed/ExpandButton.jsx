46bb6f5b68aaae8fa35bfe87b5756d92ee3ad560
Jonathan Grimes <jsg2021@gmail.com>
2021-09-07 15:23:15 -0500

    Let the icon/button determine cursor

--- src/social/parts/collapsed/ExpandButton.jsx
+++ src/social/parts/collapsed/ExpandButton.jsx
@@ -4,6 +4,7 @@ export default styled(IconContainer)`
 	background: url('../../assets/more.png');
 	background-repeat: no-repeat;
 	opacity: 0.6;
+	cursor: pointer;
 
 	&:hover {
 		opacity: 1;
--- src/social/parts/collapsed/IconContainer.jsx
+++ src/social/parts/collapsed/IconContainer.jsx
@@ -3,6 +3,5 @@ export default styled('div')`
 	margin: 8px auto;
 	width: 45px;
 	height: 45px;
-	cursor: pointer;
 	margin-left: 8px;
 `;
